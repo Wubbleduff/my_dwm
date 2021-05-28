49ca48838322f278534305485600b00255077864
Michael Fritz <michael.s.fritz@gmail.com>
2021-05-27 17:40:39 -0700

    Adding alpha

--- config.def.h
+++ config.def.h
@@ -13,10 +13,18 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const unsigned int baralpha1 = 0x04;
+static const unsigned int baralpha2 = 0x10;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1   },
+	[SchemeSel]  = { col_gray4, col_gray2, col_gray2  },
+};
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg         border     */
+	[SchemeNorm] = { OPAQUE, baralpha1, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha2, borderalpha },
 };
 
 /* tagging */
